d7bfd5d7080245433f668b7c723b39574557db70
Brigham <byerlybrigham@gmail.com>
2017-04-15 20:25:19 -0400

    Removed FRC API Key from code

--- app/src/main/java/com/walpolerobotics/scouting/scoutingserver/EventScheduleActivity.java
+++ app/src/main/java/com/walpolerobotics/scouting/scoutingserver/EventScheduleActivity.java
@@ -1,10 +1,10 @@
 package com.walpolerobotics.scouting.scoutingserver;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -17,7 +17,6 @@ import com.walpolerobotics.scouting.scoutingserver.frcapi.FRCApi;
 import com.walpolerobotics.scouting.scoutingserver.frcapi.Schedule;
 
 import java.io.File;
-import java.util.ArrayList;
 
 public class EventScheduleActivity extends AppCompatActivity {
 
